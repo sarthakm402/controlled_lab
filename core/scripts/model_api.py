2c25348b1a5afe90f6d5b5762ca103ce8e29c4ff
Sarthak Mohapatra <sarthak.mohapatra@veersatech.com>
2026-02-18 14:22:10 +0530

    made a model from scatch

---
+++ core/scripts/model_api.py
@@ -0,0 +1,96 @@
+import torch 
+import torch.nn as nn
+from fastapi import FastAPI
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score
+from sklearn.preprocessing import StandardScaler
+df=pd.read_csv("my csv path")
+x=df.drop(columns=["id","label"])
+y=df["label"]
+xtrain,xtemp,ytrain,ytemp=train_test_split(x,y,test_size=0.3,shuffle=True)
+xval,xtest,yval,ytest=train_test_split(xtemp,ytemp,test_size=0.5,shuffle=True)
+scaler=StandardScaler()
+xtrain_scaled=scaler.fit_transform(xtrain)
+xval_scaled=scaler.transform(xval)
+xtest_scaled=scaler.transform(xtest)
+
+class mydataset(Dataset):
+    def __init__(self,x,y):
+        self.x=torch.tensor(x,dtype=torch.float32)
+        self.y=torch.tensor(y,dtype=torch.float32)
+    def __len__(self,x):
+        return len(self.x)
+    def __getitem__(self, index):
+        return self.x[index],self.y[index]
+train_loader=DataLoader(mydataset(xtrain_scaled,ytrain),
+                        batch_size=254,
+                        shuffle=True)
+val_loader=DataLoader(mydataset(xval_scaled,yval),batch_size=254)
+test_loader=DataLoader(mydataset(xtest_scaled,ytest),batch_size=254)
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu" )
+class MLP(nn.Module):
+    def __init__(self,input_features):
+        super().__init__()
+        self.net=nn.Sequential(
+            nn.Linear(input_features,128),
+            nn.ReLU(),
+            nn.Linear(128,64),
+            nn.ReLU(),
+            nn.Linear(64,1)
+        )
+    def forward(self,x):
+        return self.net(x).squeeze(1)
+model=MLP(len(xtrain_scaled.shape[1])).to(device)
+criterion=nn.BCEWithLogitsLoss()
+optimiser=torch.optim.Adam(model.parameters(),lr=0.0001)
+for steps in range(1000):
+    model.train()
+    train_loss=[]
+    for xb,yb in train_loader:
+        xb, yb = xb.to(device), yb.to(device)
+        logits=model(xb)
+        loss=criterion(logits,yb)
+        loss.backward()
+        optimiser.step()
+        optimiser.zero_grad()
+        train_loss.append(loss.item())
+    model.eval()
+    val_loss=[]
+    with torch.no_grad():
+        for xb1,yb1 in val_loader:
+         xb1,yb1=xb1.to(device),yb1.to(device)
+         logits=model(xb1)
+         loss=criterion(logits,yb1)
+         val_loss.append(loss.item())
+    model.eval()
+    all_preds = []
+    all_targets = []
+
+    with torch.no_grad():
+     for xb, yb in test_loader:
+        xb, yb = xb.to(device), yb.to(device)
+        logits = model(xb)
+        probs = torch.sigmoid(logits)  
+        all_preds.extend(probs.cpu().numpy())
+        all_targets.extend(yb.cpu().numpy())
+    preds_binary = [1 if p > 0.5 else 0 for p in all_preds]
+    acc = accuracy_score(all_targets, preds_binary)
+prec = precision_score(all_targets, preds_binary)
+rec = recall_score(all_targets, preds_binary)
+roc_auc = roc_auc_score(all_targets, all_preds)
+print(f"Test Metrics -> Acc: {acc:.4f} | Prec: {prec:.4f} | Rec: {rec:.4f} | ROC-AUC: {roc_auc:.4f}")
+torch.save(model.state_dict(), "best_model.pt")
+
+
+    
+         
+
+
+
+
+
+    
+
